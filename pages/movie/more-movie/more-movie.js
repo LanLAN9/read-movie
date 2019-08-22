ce9651f30dfdf1e0a75f12be2eacefd5fc1ba35d
Lan <47134586+LanLAN9@users.noreply.github.com>
2019-08-22 16:17:24 +0800

    电影数据部分进行作出了改变

--- pages/movie/more-movie/more-movie.js
+++ pages/movie/more-movie/more-movie.js
@@ -1,34 +1,27 @@
-// pages/more-movie/more-movie.js
-var moviedata=require('../movie-data/local-data.js')
+
 Page({
 
-  
+
   data: {
 
   },
 
- 
-  onLoad: function (options) {
-    var movieBoxId = options.movieBoxId;
-    var movieTitle=options.movieTitle;
-    this.data.navigattitle = movieTitle;
 
-    var movie_data = moviedata.movieList;
-    this.setData({
-      movieData: movie_data
-    });
+  onLoad: function (options) {
+    this.moveList();
+    
   },
 
-  onReady:function(){
+  onReady: function () {
     wx.setNavigationBarTitle({
       title: this.data.navigattitle
     })
   },
-  onbindcofirm:function(event){
-    var value=event.detail.value;
+  onbindcofirm: function (event) {
+    var value = event.detail.value;
     wx.showModal({
       title: '提示',
-      content: '您搜索的内容' + value+'无法查询！！！',
+      content: '您搜索的内容' + value + '无法查询！！！',
       success(res) {
         if (res.confirm) {
           console.log('用户点击确定')
@@ -37,6 +30,26 @@ Page({
         }
       }
     });
+  },
+  moveList: function () {
+    wx.showToast({
+      title: '正在加载',
+      icon: 'loading',
+      duration: 5000
+    });
+    let thisPage = this;
+    wx.request({
+      url: 'https://douban-api.now.sh/v2/movie/top250?start=1&count=15',
+      method: 'GET',
+      header: {
+        "Content-Type": "json"
+      },
+      success: function (res) {
+        wx.hideLoading();
+        thisPage.setData({
+          lotsmovie: res.data.subjects,
+        });
+      }
+    })
   }
-  
 })
